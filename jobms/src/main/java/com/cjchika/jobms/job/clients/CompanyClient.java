99e893b5a25b341a514617a36c4692fd5f46ec29
Chika Chijioke <8pluscreative@gmail.com>
2025-01-03 16:28:09 +0100

    Message broker - RabbitMQ

--- jobms/src/main/java/com/cjchika/jobms/job/clients/CompanyClient.java
+++ jobms/src/main/java/com/cjchika/jobms/job/clients/CompanyClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "companyms")
+@FeignClient(name = "companyms", url = "${jobms.url}")
 public interface CompanyClient {
 
     @GetMapping("/api/companies/{id}")
--- jobms/src/main/java/com/cjchika/jobms/job/clients/ReviewClient.java
+++ jobms/src/main/java/com/cjchika/jobms/job/clients/ReviewClient.java
@@ -1,6 +1,5 @@
 package com.cjchika.jobms.job.clients;
 
-import com.cjchika.jobms.job.external.Company;
 import com.cjchika.jobms.job.external.Review;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "reviewms")
+@FeignClient(name = "reviewms", url = "${reviewms.url}")
 public interface ReviewClient {
 
     @GetMapping("/api/reviews")
