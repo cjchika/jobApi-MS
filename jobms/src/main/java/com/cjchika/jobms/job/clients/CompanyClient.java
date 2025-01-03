00b6e90247b9176c1cac895795d6d0e5dd3df7f7
Chika Chijioke <8pluscreative@gmail.com>
2025-01-04 00:17:02 +0100

    Dockerized Microservices

--- jobms/src/main/java/com/cjchika/jobms/job/clients/CompanyClient.java
+++ jobms/src/main/java/com/cjchika/jobms/job/clients/CompanyClient.java
@@ -1,12 +1,11 @@
 package com.cjchika.jobms.job.clients;
 
-
 import com.cjchika.jobms.job.external.Company;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "companyms", url = "${jobms.url}")
+@FeignClient(name = "companyms", url = "${companyms.url}")
 public interface CompanyClient {
 
     @GetMapping("/api/companies/{id}")
